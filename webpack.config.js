5bf5e847b0574ab801080c03f6869611c9a3091e
Patrick.Dowd <patrick.dowd@calabrio.com>
2018-02-23 14:12:15 -0600

    Housekeeping changes

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+    entry: './src/main/webapp/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    }
+};
+
+module.exports = config;
