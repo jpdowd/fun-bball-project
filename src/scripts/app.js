80ea8af3f958c2dadafaf0b51540f37ca0bd3955
pdowd <jpatrickdowd@gmail.com>
2018-04-05 19:30:03 -0500

    Changes for webpack

---
+++ src/scripts/app.js
@@ -0,0 +1,9 @@
+import * as _ from 'lodash';
+
+console.log('Hello Patrick-Start your project');
+console.log('test message');
+
+var arr=[1,2,3];
+_.each(arr, function(val){
+    console.log("Loadsh " + val);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,47 @@
 const webpack = require('webpack');
 const path = require('path');
+const package = require('./package.json');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
-    entry: './src/main/webapp/index.js',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
+    entry: {
+        app: './src/scripts/app.js',
+        vendor: Object.keys(package.dependencies)
     },
+    output: {
+        filename: './[name].bundle.js'
+    },
+    node: {
+        fs: "empty",
+        module: "empty"
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            hash: true,
+            title: 'My Baseball Project',
+            pageHeader: 'My Baseball Project',
+            template: './src/main/webapp/index.html',
+            filename: './index.html'
+        })
+    ],
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: 'babel-loader'
+            },
+            {
+                test:/\.(s*0)css$/,
+                use: ['style-loader','css-loader','sass-loader']
             }
         ]
     },
+    watch: true, 
+    resolve: {extensions: [".js", ".ts"]},
     devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        port: 2700,
-        open: true,
-        watchContentBase: true
+        contentBase: path.join(__dirname, "../dist/"),
+        port: 2700
     }
 };
 
